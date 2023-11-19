8b83332ea61161cb939071769b0898c8fda07e09
MartinWie <donotsuspend@googlegroups.com>
2023-11-19 22:01:05 +0100

    Added Mockk to project +formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val mockkVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.9.20"
@@ -31,4 +32,5 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.mockk:mockk:${mockkVersion}")
 }
